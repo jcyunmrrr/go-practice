b7be4332979ecd35d789f8877a9c8c6675d82060
jcyunmrrr <jsmn303@gmail.com>
2024-03-07 14:15:27 +0800

    add 00-hello-world

---
+++ codes/00-hello-world/hello-world.go
@@ -0,0 +1,12 @@
+// https://ithelp.ithome.com.tw/articles/10292279
+// https://medium.com/%E5%BE%AE%E5%B3%AF%E9%A3%9B%E7%BF%94/golang-go-mod-%E8%B5%B7%E6%89%8B%E5%8B%A2-39a0be969ffc
+
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Printf("Hello, world!")
+}
+
+// 20240307 
